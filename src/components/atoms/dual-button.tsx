d2c745a14c8911484af9932cd9607f72dbba4a6c
refilwe-m <refilwe.dev@gmail.com>
2023-04-04 13:45:52 +0200

    Added dual button atom

--- src/components/atoms/dual-button.tsx
+++ src/components/atoms/dual-button.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
+import { Button } from "..";
 
-export const DualButton = () => {
+type DualBtnProps = {
+  texts: string[];
+};
+
+export const DualButton = ({ texts }: DualBtnProps) => {
   return (
-    <div>DualButton</div>
-  )
-}
+    <section className="flex gap-6">
+      <Button
+        className="bg-indigo-500 w-25 text-white p-2 w-20"
+        text={texts[0]}
+      />
+      <Button
+        className="border-red-600 border text-red-600 w-20"
+        text={texts[1]}
+        variant="outline"
+      />
+    </section>
+  );
+};
