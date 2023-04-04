7b4355785a0979c15985ce5d1e096829035a8e1f
refilwe-m <refilwe.dev@gmail.com>
2023-04-04 13:43:26 +0200

    added btn base component

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { Children, ReactNode } from "react";
 
-export const Button = () => {
+type ButtonProps = {
+  text: string;
+  className?: string;
+  children?: ReactNode;
+  variant?: "filled" | "outline";
+  action?: () => {};
+};
+
+export const Button = ({
+  text,
+  className,
+  children,
+  variant = "filled",
+}: ButtonProps) => {
   return (
-    <div>Button</div>
-  )
-}
+    <button
+      className={`${className} flex gap-2 justify-center items-center ${
+        variant !== "filled" ? "border-solid bg-transparent" : ""
+      }`}
+    >
+      {children}
+      <span>{text}</span>
+    </button>
+  );
+};
