be7d6076b8b066ea5ecdf6e51376bf91ff60fe86
refilwe-m <refilwe.dev@gmail.com>
2023-04-05 02:25:08 +0200

    Import according to code structure

--- src/components/atoms/panel.tsx
+++ src/components/atoms/panel.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
-function Panel() {
-  return <div>Panel</div>;
-}
+import {PanelProps} from './types';
 
-export default Panel;
+export const Panel = ({ children, title }: PanelProps) => {
+  return (
+    <section className="panel inline-block p-2 text-white bg-gray-900">
+      {title && <h1 className="text-2xl">{title}</h1>}
+      <main className="bg-gray-700 p-3">{children}</main>
+    </section>
+  );
+};
