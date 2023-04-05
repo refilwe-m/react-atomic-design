3eb6bb3de05e26f5ea26c89a5972bace8fd54275
refilwe-m <refilwe.dev@gmail.com>
2023-04-05 11:21:25 +0200

    Molecule: Clock-Panel

--- src/components/molecules/clock-panel.tsx
+++ src/components/molecules/clock-panel.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { Panel } from "../atoms/panel";
+import { GiTimeSynchronization } from "react-icons/gi";
 
 export const ClockPanel = () => {
-  return <div>ClockPanel</div>;
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const time = `${hours}:${minutes} ${hours > 12 ? "PM" : "AM"}`;
+
+  return (
+    <Panel>
+      <>
+      <h1>Clock in time</h1>
+        <p className="text-3xl">{time}</p>
+        <section className="sync-status flex  gap-2 items-center text-green-500">
+          <GiTimeSynchronization/>
+          <p>
+             Device in sync
+          </p>
+        </section>
+      </>
+    </Panel>
+  );
 };
