b98b8d440c6b229fa90c81fe1d83b15c4517411a
refilwe-m <refilwe.dev@gmail.com>
2023-04-05 12:05:30 +0200

    Added panel colors to config

--- src/components/atoms/panel.tsx
+++ src/components/atoms/panel.tsx
@@ -4,9 +4,11 @@ import { PanelProps } from "./types";
 
 export const Panel = ({ children, title }: PanelProps) => {
   return (
-    <section className="panel inline-block p-2 text-white bg-gray-900">
-      {title && <h1 className="text-2xl">{title}</h1>}
-      <main className="bg-gray-700 p-3">{children}</main>
+    <section className="panel inline-block p-3 text-white bg-dark-panel-bg">
+      {title && <h1 className="text-1xl pb-5">{title}</h1>}
+      <main className="bg-dark-panel-sub-bg drop-shadow-xl rounded sm p-3">
+        {children}
+      </main>
     </section>
   );
 };
