f458a125e32ebc6fed23cb1ad548162acef30247
refilwe-m <refilwe.dev@gmail.com>
2023-04-06 08:50:50 +0200

    Added imports on App

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+
+import {
+  Header,
+  Button,
+  ErrorText,
+  Avatar,
+  ClockPanel,
+  FormPanel,
+  JobPanel,
+} from "./components";
 
 function App() {
-  
-  return (
-    <div className="App">
-      <button className='bg-green-600'>Hello</button>
-    </div>
-  )
+  return <div className="App flex justify-around pt-10"></div>;
 }
 
-export default App
+export default App;
