716bf454b48d1e2e834862c963084d154ed6c9bf
refilwe-m <refilwe.dev@gmail.com>
2023-04-06 11:05:59 +0200

    removed custom colors

--- src/components/atoms/panel.tsx
+++ src/components/atoms/panel.tsx
@@ -4,11 +4,9 @@ import { PanelProps } from "./types";
 
 export const Panel = ({ children, title, className }: PanelProps) => {
   return (
-    <section
-      className={`panel inline-block p-3 text-white bg-dark-panel-bg ${className}`}
-    >
+    <section className={`panel inline-block p-3 bg-[#141517] ${className}`}>
       {title && <h1 className="text-1xl pb-5">{title}</h1>}
-      <main className="bg-dark-panel-sub-bg drop-shadow-xl rounded sm p-3">
+      <main className="bg-[#191a1f] drop-shadow-xl rounded sm p-3">
         {children}
       </main>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,6 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      dark: "#191919",
-      transparent: "transparent",
-      current: "currentColor",
-      white: "#ffffff",
-      "dark-panel-bg": "#141517",
-      "dark-panel-sub-bg": "#191a1f",
-    },
     extend: {},
   },
   plugins: [],
