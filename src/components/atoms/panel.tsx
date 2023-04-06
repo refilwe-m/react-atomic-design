eb7a6241e34dce88faf2031a9d17563ca8728f2c
refilwe-m <refilwe.dev@gmail.com>
2023-04-06 11:10:52 +0200

    unlinked custom styles

--- src/components/atoms/panel.tsx
+++ src/components/atoms/panel.tsx
@@ -4,7 +4,9 @@ import { PanelProps } from "./types";
 
 export const Panel = ({ children, title, className }: PanelProps) => {
   return (
-    <section className={`panel inline-block p-3 bg-[#141517] ${className}`}>
+    <section
+      className={`panel inline-block text-white p-3 bg-[#141517] ${className}`}
+    >
       {title && <h1 className="text-1xl pb-5">{title}</h1>}
       <main className="bg-[#191a1f] drop-shadow-xl rounded sm p-3">
         {children}
