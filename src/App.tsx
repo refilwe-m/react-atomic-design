3e9b9c11d5a791e4f04851859bff328a3341c545
refilwe-m <refilwe.dev@gmail.com>
2023-04-06 16:12:36 +0200

    Displayed some Atoms

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,27 @@ import {
 } from "./components";
 
 function App() {
-  return <div className="App flex justify-around pt-10"></div>;
+  return (
+    <div className="App pt-10">
+      <Header
+        title="Atoms"
+        className="underline text-white text-6xl font-bold text-center mb-8"
+      />
+
+      <section className="flex justify-evenly">
+        <Avatar imgSrc="https://picsum.photos/200" onlineStatus="Online" />
+        <Avatar imgSrc="https://picsum.photos/200" />
+        <Avatar imgSrc="https://picsum.photos/200" onlineStatus="Offline" />
+      </section>
+
+      <section className="flex justify-evenly mt-8">
+        <Button text="Default" />
+        <Button text="Outline" className="px-3 py-1" variant="outline" />
+        <Button text="Text" variant="text" />
+        <Button text="Outline with Delete" func="delete" variant="outline" />
+      </section>
+    </div>
+  );
 }
 
 export default App;
