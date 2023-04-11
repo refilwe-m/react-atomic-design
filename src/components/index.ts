76cf48961dca7351b928d517d4e235507ef4c92e
refilwe-m <refilwe.dev@gmail.com>
2023-04-11 06:17:08 +0200

    Organisms: Registration & Login

--- src/components/index.ts
+++ src/components/index.ts
@@ -10,3 +10,6 @@ export * from "./molecules/form-panel";
 export * from "./molecules/user-panel";
 export * from "./molecules/input-field";
 export * from "./molecules/job-panel";
+
+export * from "./organisms/login-form";
+export * from "./organisms/register-form";
--- src/components/organisms/login-form.tsx
+++ src/components/organisms/login-form.tsx
@@ -1,5 +1,42 @@
 import React from "react";
 
+import { AuthPanel, Button, InputField, Header } from "..";
+
 export const LoginForm = () => {
-  return <div>LoginForm</div>;
+  return (
+    <AuthPanel className="p-12">
+      <Header
+        className="text-3xl font-extrabold text-white my-8 text-center font-momo"
+        title="Codehesion Rockets"
+      />
+      <form className="text-black flex flex-col justify-center gap-8">
+        <InputField
+          color="text-black"
+          className="placeholder:font-extrabold placeholder:text-gray-600"
+          placeholder="Username"
+        />
+        <section>
+          <InputField
+            color="text-gray-600"
+            placeholder="Password"
+            type="password"
+            className="placeholder:font-extrabold placeholder:text-gray-600 mb-2"
+          />
+          <section className="flex justify-between text-xs">
+            <label className="text-white" htmlFor="remember-me">
+              <input className="mr-3" name="remember-me" type="checkbox" />
+              Keep me signed in
+            </label>
+            <Button
+              className="text-blue-600 text-xs"
+              variant="text"
+              text="Forgot Password?"
+            />
+          </section>
+        </section>
+
+      <Button className="w-60 text-3xl" text="Login" />
+      </form>
+    </AuthPanel>
+  );
 };
