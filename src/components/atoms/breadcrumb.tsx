171a4d2d8b64c139c9112dd99904fb9ea637e95f
refilwe-m <refilwe.dev@gmail.com>
2023-04-13 13:53:52 +0200

    Added breadcrumb

---
+++ src/components/atoms/breadcrumb.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { BsChevronDoubleRight } from "react-icons/bs";
+import { Link, useLocation, matchRoutes } from "react-router-dom";
+
+export const Breadcrumb = () => {
+  const url = useLocation();
+  const currentPath = url.pathname;
+
+  const listItems = (
+    <li
+      className={`${
+        /*  path === currentPath && */ "text-blue-700 font-semibold"
+      } flex items-center hover:text-blue-400`}
+    >
+      <Link to={currentPath}>{currentPath.slice(1)}</Link>
+      <BsChevronDoubleRight className="ml-5" />
+    </li>
+  );
+  return (
+    <section className="breadcrumb p-2 text-white">
+      <ul className="flex gap-10">{listItems}</ul>
+    </section>
+  );
+};
--- src/components/templates/app-layout.tsx
+++ src/components/templates/app-layout.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 import { ChildrenProps } from "../../common/types";
+import { Breadcrumb } from "..";
 
 export const AppLayout = ({ children }: ChildrenProps) => {
-  return <section className="w-screen h-screen pt-6 px-12">{children}</section>;
+  return (
+    <section className="w-screen h-screen pt-6 px-12">
+      <Breadcrumb />
+      {children}
+    </section>
+  );
 };
