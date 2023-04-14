4deb9cae3fca60c09c4ce2b61a0df462372b8c9e
refilwe-m <refilwe.dev@gmail.com>
2023-04-14 16:24:41 +0200

    Nav: Fake Breadcrumb

--- src/components/atoms/breadcrumb.tsx
+++ src/components/atoms/breadcrumb.tsx
@@ -5,20 +5,21 @@ import { Link, useLocation, matchRoutes } from "react-router-dom";
 export const Breadcrumb = () => {
   const url = useLocation();
   const currentPath = url.pathname;
+  const routes = ["/login", "/register", "/profile", "/form", "/report"];
 
-  const listItems = (
+  const listItems = routes.map((route) => (
     <li
-      className={`${
-        /*  path === currentPath && */ "text-blue-700 font-semibold"
+      className={` ${
+        route == currentPath && "text-blue-700 font-semibold"
       } flex items-center hover:text-blue-400`}
     >
-      <Link to={currentPath}>{currentPath.slice(1)}</Link>
+      <Link to={route}>{route.slice(1)}</Link>
       <BsChevronDoubleRight className="ml-5" />
     </li>
-  );
+  ));
   return (
     <section className="breadcrumb p-2 text-white">
       <ul className="flex gap-10">{listItems}</ul>
     </section>
   );
-  
+};
